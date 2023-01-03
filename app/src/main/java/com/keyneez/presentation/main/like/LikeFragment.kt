5958fa028c18b5c1197ef308d0e465cbefd86b14
0zlrlo <99941493+0zlrlo@users.noreply.github.com>
2023-01-03 21:37:26 +0900

    [UI/#5] 저장 화면 ui 구현

---
+++ app/src/main/java/com/keyneez/presentation/main/like/LikeFragment.kt
@@ -0,0 +1,13 @@
+package com.keyneez.presentation.main.like
+
+import android.os.Bundle
+import android.view.View
+import com.keyneez.util.binding.BindingFragment
+import com.lab.keyneez.R
+import com.lab.keyneez.databinding.FragmentLikeBinding
+
+class LikeFragment : BindingFragment<FragmentLikeBinding>(R.layout.fragment_like) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
