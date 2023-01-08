ad74c2acd8384f2d2346feb918722264aed8678e
tama <readyjoo24@gmail.com>
2023-01-08 16:45:43 +0900

    [ADD/#20] 상세 뷰모델 추가
    +rename

--- app/src/main/java/com/keyneez/presentation/main/detail/DetailActivity.kt
+++ app/src/main/java/com/keyneez/presentation/main/detail/DetailActivity.kt
@@ -5,9 +5,18 @@ import com.keyneez.util.binding.BindingActivity
 import com.lab.keyneez.R
 import com.lab.keyneez.databinding.ActivityHomeDetailBinding
 
-class HomeDetailActivity :
+class DetailActivity :
     BindingActivity<ActivityHomeDetailBinding>(R.layout.activity_home_detail) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
+
+    // 뒤로가기 버튼
+    private fun initBackClickListener(){
+
+    }
+    // 공유 버튼
+    private fun
+    // 좋아요 버튼
+
 }
---
+++ app/src/main/java/com/keyneez/presentation/main/detail/DetailViewModel.kt
@@ -0,0 +1,6 @@
+package com.keyneez.presentation.main.detail
+
+import androidx.lifecycle.ViewModel
+
+class DetailViewModel : ViewModel(){
+}
