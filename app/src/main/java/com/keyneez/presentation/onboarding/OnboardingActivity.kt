a18d86a9f987944978e9d1b72b64abc0a5a9f1bb
tama <readyjoo24@gmail.com>
2023-01-09 22:39:51 +0900

    [FEAT/#14] 회원가입/로그인 버튼 visibility 설정

--- app/src/main/java/com/keyneez/presentation/onboarding/OnboardingActivity.kt
+++ app/src/main/java/com/keyneez/presentation/onboarding/OnboardingActivity.kt
@@ -1,15 +1,19 @@
 package com.keyneez.presentation.onboarding
 
 import android.os.Bundle
-import com.keyneez.presentation.onboarding.adapter.OnboardingAdapter
+import androidx.activity.viewModels
+import androidx.viewpager2.widget.ViewPager2
 import com.keyneez.util.binding.BindingActivity
 import com.lab.keyneez.R
 import com.lab.keyneez.databinding.ActivityOnboardingBinding
 
 class OnboardingActivity :
     BindingActivity<ActivityOnboardingBinding>(R.layout.activity_onboarding) {
+    private val viewModel: OnboardingViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding.viewModel = viewModel
 
         initOnboardingViewPager()
     }
@@ -20,10 +24,16 @@ class OnboardingActivity :
         viewPager.adapter = OnboardingAdapter(this)
         dotIndicator.attachTo(viewPager)
 
-//        if (viewPager.currentItem == 3) {
-//            // 로그인, 회원가입 버튼 활성화
-//            binding.btnOnboardingLogin.setVisibility(View.VISIBLE)
-//            binding.btnOnboardingSignup.setVisibility(View.VISIBLE)
-//        }
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels)
+
+                viewModel.setPosition(position)
+            }
+        })
     }
 }
