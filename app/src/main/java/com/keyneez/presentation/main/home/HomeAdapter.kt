2474675ed78363cb815b1c21362e84f63756a609
tama <readyjoo24@gmail.com>
2023-01-10 22:29:28 +0900

    [UI/#7]  margin값 수정

--- app/src/main/java/com/keyneez/presentation/main/home/HomeAdapter.kt
+++ app/src/main/java/com/keyneez/presentation/main/home/HomeAdapter.kt
@@ -4,8 +4,6 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.keyneez.presentation.main.home.neww.NewFragment
-import com.keyneez.presentation.main.home.popular.PopularFragment
 import com.keyneez.presentation.main.home.recommend.RecommendFragment
 
 class HomeAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
@@ -15,11 +13,11 @@ class HomeAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
     }
 
     override fun createFragment(position: Int): Fragment {
-        when (position) {
-            0 -> return RecommendFragment()
-            1 -> return PopularFragment()
-            2 -> return NewFragment()
-        }
+//        when (position) {
+//            0 -> return RecommendFragment()
+//            1 -> return PopularFragment()
+//            2 -> return NewFragment()
+//        }
         return RecommendFragment()
     }
 }
