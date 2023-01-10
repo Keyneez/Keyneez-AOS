ad4ddb84a6e09d1ebe56d54b6589208b5febeb01
b1urrrr <b1urrr@naver.com>
2023-01-10 23:22:13 +0900

    [FIX/#35] ServiceModule 파일명 통일

--- app/src/main/java/com/keyneez/di/ServiceModule.kt
+++ app/src/main/java/com/keyneez/di/ServiceModule.kt
@@ -10,7 +10,7 @@ import retrofit2.Retrofit
 
 @Module
 @InstallIn(SingletonComponent::class)
-object RetrofitServiceModule {
+object ServiceModule {
     @Provides
     fun providesContentService(retrofit: Retrofit): ContentService =
         retrofit.create(ContentService::class.java)
